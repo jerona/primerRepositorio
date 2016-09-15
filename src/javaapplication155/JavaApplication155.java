965bfeff15bbf99a621356c84aec94a842ecd7a0
Joel <Joel@Microsoft-3454>
2016-09-15 15:05:25 +0100

    texto de prueba

--- src/javaapplication155/JavaApplication155.java
+++ src/javaapplication155/JavaApplication155.java
@@ -17,6 +17,7 @@ public class JavaApplication155 {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
         System.out.println("asdsadsadasd");
+        System.out.println("texto base");
     }
     
 }
